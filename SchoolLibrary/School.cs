e39e5d484ff4e753b8574be40f78711205e78cf2
Javier Ramirez <lorlgd1@gmail.com>
2019-08-28 19:03:19 -0500

    JaRa: Added methods

--- SchoolLibrary/School.cs
+++ SchoolLibrary/School.cs
@@ -4,7 +4,50 @@ using System.Text;
 
 namespace SchoolLibrary
 {
-    class School
+    public class School
     {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string PhoneNumber { get; set; }
+
+        private string _twitterAddress;
+        public string TwitterAddress
+        {
+            //make sure the twitter address starts with @
+            get { return _twitterAddress; }
+            set
+            {
+                if(value.StartsWith("@"))
+                {
+                    _twitterAddress = value;
+                }
+                else
+                {
+                    throw new Exception("The twitter address must beggin with @");
+                }
+            }
+        }
+
+        public School()
+        {
+            Name = "Untitled School";
+            PhoneNumber = "555-1234";
+
+        }
+
+        public School(string SchoolName, string SchoolPhoneNumber)
+        {
+            Name = SchoolName;
+            PhoneNumber = SchoolPhoneNumber;
+        }
+
+        public float AverageThreeScores(float a, float b, float c)
+        {
+            var result = (a + b + c) / 3;
+            return result
+        }
     }
 }
